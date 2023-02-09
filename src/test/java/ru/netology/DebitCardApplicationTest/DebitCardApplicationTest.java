5d0ffcdec290aaece45b1b3e82d4f83b304f593d
Светлана Алексеева <Lanaleks90@gmail.com>
2023-02-09 21:55:02 +0400

    Adding task 2

--- src/test/java/ru/netology/DebitCardApplicationTest/DebitCardApplicationTest.java
+++ src/test/java/ru/netology/DebitCardApplicationTest/DebitCardApplicationTest.java
@@ -40,7 +40,7 @@ public class DebitCardApplicationTest {
     }
 
     @Test
-    void shouldTest() {
+    void shouldTestSuccessfulSending() {
         driver.get("http://localhost:9999/");
         List<WebElement> list = driver.findElements(By.className("input__control"));
         list.get(0).sendKeys("Пугачева Алла");
@@ -50,5 +50,52 @@ public class DebitCardApplicationTest {
         driver.findElement(By.className("button__text")).click();
         String text = driver.findElement(By.className("paragraph")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+
+    @Test
+    void shouldTestIncorrectName() {
+        driver.get("http://localhost:9999/");
+        List<WebElement> list = driver.findElements(By.className("input__control"));
+        list.get(0).sendKeys("Petrovskaya Inna");
+        list.get(1).sendKeys("+79357484905");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
+        String text = driver.findElement(By.className("input__sub")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+    }
+
+    @Test
+    void shouldTestIncorrectTelephone() {
+        driver.get("http://localhost:9999/");
+        List<WebElement> list = driver.findElements(By.className("input__control"));
+        list.get(0).sendKeys("Иванова Наталья");
+        list.get(1).sendKeys("+793574849059");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
+        list = driver.findElements(By.className("input__sub"));
+        String text = list.get(1).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+    }
+
+    @Test
+    void shouldTestEmptyCheckbox() {
+        driver.get("http://localhost:9999/");
+        List<WebElement> list = driver.findElements(By.className("input__control"));
+        list.get(0).sendKeys("Иванова Наталья");
+        list.get(1).sendKeys("+793574849059");
+        driver.findElement(By.className("button__text")).click();
+        String text = driver.findElement(By.className("checkbox__text")).getText();
+        assertEquals("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй", text.trim());
+    }
+
+    @Test
+    void shouldTestEmptyForm() {
+        driver.get("http://localhost:9999/");
+        List<WebElement> list = driver.findElements(By.className("input__control"));
+        list.get(0).sendKeys("");
+        list.get(1).sendKeys("");
+        driver.findElement(By.className("button__text")).click();
+        String text = driver.findElement(By.className("input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", text.trim());
     }
 }
