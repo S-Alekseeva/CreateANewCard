f48693c2d9c916c48eb8b0a7479c492053e961cf
Светлана Алексеева <Lanaleks90@gmail.com>
2023-02-09 18:20:07 +0400

    Checking the build 2

--- src/test/java/ru/netology/DebitCardApplicationTest/DebitCardApplicationTest.java
+++ src/test/java/ru/netology/DebitCardApplicationTest/DebitCardApplicationTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.List;
 
@@ -24,8 +25,12 @@ public class DebitCardApplicationTest {
     }
 
     @BeforeEach
-    void setUp() {
-        driver = new ChromeDriver();
+    void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
